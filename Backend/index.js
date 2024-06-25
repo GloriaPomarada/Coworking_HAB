7397a076321f5056d9df24f214153da56c9fcc36
Roberto Medrano <medranoisaul5@gmail.com>
2024-06-25 18:49:18 +0200

    Servidor express

--- Backend/index.js
+++ Backend/index.js
@@ -1,2 +1,12 @@
 import express from "express";
 import path from "path";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("¡Hola, mundo!");
+});
+
+app.listen(3000, () => {
+  console.log("Servidor escuchando en el puerto 3000");
+});
