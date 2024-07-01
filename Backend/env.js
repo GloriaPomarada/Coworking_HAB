13e64f0d62ab70ad5b1c9f58a9b5cc107099c3eb
Gloria Pomarada <gpomarada@gmail.com>
2024-07-01 19:49:00 +0200

    script initDB añadido

--- Backend/env.js
+++ Backend/env.js
@@ -1,18 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-const { 
-    PORT,
-    DB_HOST,
-    DB_USER,
-    DB_PASSWORD,
-    DB_NAME,
-} = process.env;
+const { PORT, DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, SECRET } = process.env;
 
-export { 
-    PORT,
-    DB_HOST,
-    DB_USER,
-    DB_PASSWORD,
-    DB_NAME
- };
+export { PORT, DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, SECRET };
