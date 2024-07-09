d168f4a65686af8a041ebeac1c167d03e156b0e3
Gloria Pomarada <gpomarada@gmail.com>
2024-07-09 12:50:58 +0200

    endpoint modificación de espacios

--- Backend/src/models/spaces/index.js
+++ Backend/src/models/spaces/index.js
@@ -1,2 +1,3 @@
 export { default as filteredSpaces } from './spaceFilterModel.js';
 export { default as createSpace } from './createSpaceModel.js';
+export { default as updateSpace } from './updateSpaceModel.js';
--- Backend/src/routes/spaceRoutes.js
+++ Backend/src/routes/spaceRoutes.js
@@ -4,7 +4,13 @@ import * as spaceController from '../controllers/spaces/index.js';
 import authenticateMiddleware from '../middlewares/authenticateMiddleware.js';
 import isAdmin from '../middlewares/isAdmin.js';
 
-router.get('/', spaceController.getFilterSpace);//.com/api/spaces?filter=param
+router.get('/', spaceController.getFilterSpace); //.com/api/spaces?filter=param
 router.post('/', authenticateMiddleware, isAdmin, spaceController.createSpace); //-> .com/crear-espacio
+router.put(
+    '/:id',
+    authenticateMiddleware,
+    isAdmin,
+    spaceController.updateSpace
+); //-> .com/actualizar-espacio/:espacioId
 
 export default router;
