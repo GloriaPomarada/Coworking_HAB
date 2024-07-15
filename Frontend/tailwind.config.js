e9192957401a755e85f53103792ce7192bc47d48
Gloria Pomarada <gpomarada@gmail.com>
2024-07-15 11:55:47 +0200

    postman ok y setup tailwind

---
+++ Frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+//Add the paths to all of your template files
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
