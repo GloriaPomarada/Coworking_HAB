3056ea4142a95c8585326d6eee782370aaecbd3f
Roberto Medrano <medranoisaul5@gmail.com>
2024-07-22 19:33:54 +0200

    Acutalizacion

--- Backend/env.js
+++ Backend/env.js
@@ -2,17 +2,17 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const {
-    PORT = 3001,
-    DB_HOST = 'localhost',
-    DB_USER = 'root',
-    DB_PASSWORD = '7479268',
-    DB_NAME = 'coworking_db',
+    PORT,
+    DB_HOST,
+    DB_USER,
+    DB_PASSWORD,
+    DB_NAME,
     SMTP_HOST,
     SMTP_PORT,
     SMTP_USER,
     SMTP_APIKEY,
-    JWT_SECRET = 'xsmtpsib-54923885e8a92c38ab4b7b5b6546146ea4329cef55a51be2ef2f8cc2023bdf3f-wA2fv3HXJVzDLsyZ',
-    JWT_EXP = '7d',
+    JWT_SECRET,
+    JWT_EXP,
     UPLOADS_DIR,
 } = process.env;
 
